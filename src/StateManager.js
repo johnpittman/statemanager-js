cdc9318ad193a8ec0af006d522af7be4da8da0a0
John Pittman <johnrichadrpittman@gmail.com>
2014-11-19 11:11:55 -0500

    v1.0.0

--- src/StateManager.js
+++ src/StateManager.js
@@ -1,23 +1,25 @@
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD
-        define(['./State.min.js'], factory);
+        define(['./../../eventhandler/dist/EventHandler.min.js'], factory);
     } else if (typeof exports === 'object') {
         // Node, CommonJS-like
-        module.exports = factory(require('./State'));
+        module.exports = factory(require('eventhandler'));
     } else {
         // Browser globals (root is window)
-        // State.js need to be included before StateManager.
-        root.StateManager = factory(root.StateManager.State);
+        // Require EventHandler.js to be loaded before.
+        root.StateManager = factory(root.EventHandler);
     }
-}(this, function(State) {
+}(this, function(EventHandler) {
     'use strict'
 
     /**
      * @contructor
-     * @param {object} owner=this - Object of what owns the states of the state manager. Used to mantain scope.
+     * @param {object} [owner=this] - Object of what owns the states of the state manager. Used to mantain scope.
      */
     function StateManager(owner) {
+        EventHandler.call(this);
+
         this._owner = owner || this;
 
         this._states = {};
@@ -26,36 +28,92 @@
         this._initialState;
     }
 
-    StateManager.State = State;
+    StateManager.prototype = Object.create(EventHandler.prototype);
+    StateManager.contructor = StateManager;
 
     /**
-     * Adds a state object to be managed.
-     * @param {string} name
-     * @param {StateManager.State} state - State object that contains all of the process methods.
-     * @param {boolean} [isInitial] - If true, sets the state as the initial start state.
+     * Initializes the states collection with more states.
+     * Every state already has a 'Default' event
+     * @param {object} states
+     * @param {string} [initialState]
      */
-    StateManager.prototype.addState = function(name, state, isInitial) {
-        this._states[name] = state;
+    StateManager.prototype.addStates = function(states, initialState) {
+        for (var state in states) {
+            this._states[state] = states[state];
+        }
 
-        if (isInitial !== undefined) {
-            this._initialState = name;
-            this.setCurrentState(initialState);
+        this.start(initialState);
+    };
+
+    /**
+     * Runs all enter processes for a state.
+     * @param  {string} name
+     * @param  {string} data - Additional info to pass through to processes and listeners.
+     */
+    StateManager.prototype.enterState = function(fromState, data) {
+        var state = this._states[fromState];
+        if (state !== undefined) {
+            this.setCurrentState(fromState);
+
+            var enterFrom = state.transitions['onEnterFrom' + this._prevState];
+            if (enterFrom !== undefined) {
+                // beforeEnterFrom should only exist if there's an enterFrom process.
+                var beforeEnterFrom = state.transitions['onBeforeEnterFrom' + this._prevState];
+                if (beforeEnterFrom !== undefined)
+                    beforeEnterFrom.call(this._owner, data);
+                enterFrom.call(this._owner, data);
+            }
+
+            var beforeEnter = state.transitions['onBeforeEnter'];
+            if (beforeEnter !== undefined)
+                beforeEnter.call(this._owner, data);
+            this.emit('enterstate', data);
+            state['enter'].call(this._owner, data);
         }
     };
 
-    StateManager.prototype.enterState = function(name) {
-        var state = this._states[state.name];
-        if (state !== undefined) {
-            var enterFrom = state['enter:from:' + name];
+    /**
+     * Runs all leave processes for a state.
+     * @param  {string} name
+     */
+    StateManager.prototype.leaveState = function(toState, data) {
+        var state = this._states[this._currentState];
 
-            if (enterFrom !== undefined) {
-                var beforeEnterFrom = state['before:enter:from:' + name];
-                if (beforeEnterFrom !== undefined)
-                    beforeEnterFrom.call(this._owner);
-                enterFrom.call(this._owner);
-            }
-            state['enter'].call(this._owner);
+        var leaveTo = state.transitions['onLeaveTo' + toState];
+        if (leaveTo !== undefined) {
+            // beforeLeaveTo should only exist if there's an leaveTo process.
+            var beforeLeaveTo = state.transitions['onBeforeLeaveTo' + toState];
+            if (beforeLeaveTo !== undefined)
+                beforeLeaveTo.call(this._owner);
+            leaveTo.call(this._owner);
         }
+
+        var beforeLeave = state.transitions['onBeforeLeave'];
+        if (beforeLeave !== undefined)
+            beforeLeave.call(this._owner);
+        this.emit('leavestate', data);
+        state['leave'].call(this._owner);
+    };
+
+    /**
+     * Updates the current state to the state that's passed in.
+     * Emits all events.
+     * @param  {string} state
+     * @param  {*} [data] - Data to be access by all event listeners.
+     */
+    StateManager.prototype.changeState = function(toState, data) {
+        var prevState = this._prevState;
+        var currState = this._currentState;
+
+        var info = {
+            from: currState,
+            to: toState,
+            data: data
+        };
+
+        if (currState !== this._prevState)
+            this.leaveState(toState, info);
+        this.enterState(toState, info);
     };
 
     /**
@@ -64,10 +122,10 @@
      * @param {string} name
      */
     StateManager.prototype.setCurrentState = function(name) {
-        var currState = this._currentState;
-        if (currState !== undefined)
-            if (this._prevState !== currState)
-                this._prevState = currState.toString();
+        if (this._currentState !== undefined)
+            this._prevState = this._currentState.toString();
+        else
+            this._prevState = name;
         this._currentState = name;
     };
 
@@ -75,12 +133,27 @@
      * Sets the state machine back to the initial state.
      * @param {string} initialStateName
      */
-    StateManager.prototype.start = function(initialStateName) {
-        if (initialStateName !== undefined) {
-            this.setCurrentState(initialStateName);
-            this._prevState = undefined;
-        }
-        //this.changeState(this._initialState);
+    StateManager.prototype.start = function(initialState) {
+        var defaultState = this._initialState = initialState || this._initialState;
+
+        this.setCurrentState(defaultState);
+        this.changeState(defaultState);
+    };
+
+    /**
+     * Accessor.
+     * @return {string}
+     */
+    StateManager.prototype.getCurrentState = function() {
+        return this._currentState;
+    };
+
+    /**
+     * Accessor.
+     * @return {string}
+     */
+    StateManager.prototype.getPreviousState = function() {
+        return this._prevState;
     };
 
     return StateManager;
--- test/StateManagerSpec.js
+++ test/StateManagerSpec.js
@@ -4,8 +4,10 @@ var StateManager = require('../src/StateManager');
 
 describe("StateManager", function() {
     var stateManager;
+    var count = 0;
 
     beforeEach(function() {
         stateManager = new StateManager();
+        count = 0;
     });
 });
